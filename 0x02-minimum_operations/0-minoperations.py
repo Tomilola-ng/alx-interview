759b97d380b8cd16f29e2f87085ab1d492f768d1
Tomilola-ng <tee.o2809@gmail.com>
2024-07-11 22:55:02 +0100

    WHYNING_CODE

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Minimum Operations to Obtain H Characters
+
+This module provides a method to calculate the fewest number of operations
+needed to generate exactly 'n' H characters in a file.
+
+Function:
+    def min_operations(target_count: int) -> int
+"""
+
+def min_operations(target_count: int) -> int:
+    """
+    Calculate the minimum number of operations required to achieve exactly
+    'target_count' H characters in a file.
+
+    Args:
+    target_count (int): The desired number of H characters.
+
+    Returns:
+    int: The minimum number of operations, or 0 if it's impossible to achieve
+         'target_count' H characters.
+    """
+    operation_count = 0
+    factor = 2
+
+    while target_count > 1:
+        while target_count % factor == 0:
+            operation_count += factor
+            target_count //= factor
+        factor += 1
+
+    return operation_count
